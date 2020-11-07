60771d5c14182750b30b171ce01933d062977caa
Andrei911 <andrey_gk911@mail.ru>
2020-11-07 15:40:26 +0100

    add hmburger and JS

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,11 +1,3 @@
-$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() { /* открывает страницу табов */
-	$(this)
-		.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-		.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-
-		
-});
-
 window.addEventListener('DOMContentLoaded', () => {
 	const menu = document.querySelector('.navbar__menu'),
 	menuItem = document.querySelectorAll('.menu_item'),
@@ -23,8 +15,3 @@ window.addEventListener('DOMContentLoaded', () => {
 		})
 	})
 });
-
-
-
-	
-	
