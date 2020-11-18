992e1a5a5b94f0a0d41579e2ea62a74abfb21423
Andrei911 <andrey_gk911@mail.ru>
2020-11-18 22:21:14 +0100

    add object

---
+++ src/js/objects_carousel.js
@@ -0,0 +1,39 @@
+$('.objects__img__carousel').slick({
+	speed: 1500,
+	adaptiveHeight: true,
+	autoplay: false,
+	autoplaySpeed:1500,
+	prevArrow: '<button type="button" class="slick-prev"><img src=img/carusel/left.svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><img src=img/carusel/rigth.svg></button>',
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				dots: false ,
+				arrows: false 
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				dots: false,
+				arrows: false 
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				dots: false ,
+				arrows: false 
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				dots: false,
+				arrows: false 
+			}
+		}
+	]
+
+});
